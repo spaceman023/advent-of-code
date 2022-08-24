51ddf3a6f87d936151dc4d7579541557dfb04874
John Richardson <jkrichardson08@gmail.com>
2022-08-23 21:14:48 -0500

    lazy

--- 2018-javascript/day07.js
+++ 2018-javascript/day07.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("day07-input.txt")
+const input = fs.readFileSync("./inputs/day07-input.txt")
 const lines = input.toString().split("\n")
 lines.pop()
 let dic = {};
@@ -41,4 +41,28 @@ while (q.length > 0) {
     q = [...q, ...dic[next][0]]
   }
 }
-console.log(final)
+let work = final.split("")
+work = work.map(i => [i, i.charCodeAt(0) - 4])
+let workers = 5
+let working = []
+let finished = []
+let seconds = 0;
+while (work.length > 0 || working.length > 0) {
+  seconds++
+  addwork: for (let f of work) {
+    if (working.length == workers) break;
+    let required = dic[f[0]][1]
+    let testarr = finished.map(i => i[0])
+    for (let r of required) {
+      if (!testarr.includes(r)) continue addwork
+    }
+    working.push(f)
+  }
+  let worktest = working.map(j => j[0])
+  work = work.filter(i => !worktest.includes(i[0]))
+  working.forEach(i => i[1]--)
+  let done = working.filter(i => i[1] < 1)
+  finished = finished.concat(done);
+  working = working.filter(i => i[1] > 0)
+}
+console.log(seconds)
