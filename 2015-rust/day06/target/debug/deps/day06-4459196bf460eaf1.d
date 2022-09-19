17c2c038d11c25cd2be808b68560d986d3e59437
John Richardson <jkrichardson08@gmail.com>
2022-09-18 20:10:06 -0500

    commit

--- 2015-rust/day06/target/debug/deps/day06-4459196bf460eaf1.d
+++
@@ -1,6 +0,0 @@
-/Users/johnrichardson/Documents/projects/advent-of-code/2015-rust/day06/target/debug/deps/day06-4459196bf460eaf1.rmeta: src/main.rs src/input.txt
-
-/Users/johnrichardson/Documents/projects/advent-of-code/2015-rust/day06/target/debug/deps/day06-4459196bf460eaf1.d: src/main.rs src/input.txt
-
-src/main.rs:
-src/input.txt:
